3193adbfdc78bb412c48ded90706a9e525575b12
Justo Miguel Vargas <jvargas@cleo.localdomain>
2015-09-30 16:01:27 -0300

    Agregado libreria

--- app/src/main/java/com/appedidos/parseinnovaapp/MainActivity.java
+++ app/src/main/java/com/appedidos/parseinnovaapp/MainActivity.java
@@ -1,9 +1,26 @@
 package com.appedidos.parseinnovaapp;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nanotasks.BackgroundWork;
+import com.nanotasks.Completion;
+import com.nanotasks.Tasks;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +28,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Tasks.executeInBackground(this, new BackgroundWork<List<ParseObject>>() {
+            @Override
+            public List<ParseObject> doInBackground() throws Exception {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Alumnos");
+                try {
+                    return query.find();
+                } catch (ParseException e) {
+                    return null;
+                } // expensive operation
+            }
+        }, new Completion<List<ParseObject>>() {
+            @Override
+            public void onSuccess(Context context, List<ParseObject> result) {
+                LinearLayout container = (LinearLayout)findViewById(R.id.container);
+
+                for(ParseObject alumno:result){
+                    //infle vista
+                    View child = getLayoutInflater().inflate(R.layout.alumnos, null);
+                    //obtener label
+                    TextView label = (TextView) child.findViewById(R.id.nombre);
+
+                    String nombre = alumno.getString("nombre");
+
+                    label.setText(nombre);
+
+                    container.addView(child);
+                }
+            }
+
+            @Override
+            public void onError(Context context, Exception e) {
+                Toast.makeText(context, "Se produjo un error en la llamada a parse", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //new GetUsersOnBackGround().execute();
     }
 
     @Override
@@ -34,4 +89,42 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class GetUsersOnBackGround extends AsyncTask<Void, Integer,  List<ParseObject>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        protected List<ParseObject> doInBackground(Void... urls) {
+            ParseQuery<ParseObject> query = ParseQuery.getQuery("Alumnos");
+            try {
+                return query.find();
+            } catch (ParseException e) {
+                return null;
+            }
+        }
+
+        protected void onPostExecute(List<ParseObject> result) {
+            if (!MainActivity.this.isFinishing()){
+                LinearLayout container = (LinearLayout)findViewById(R.id.container);
+
+                for(ParseObject alumno:result){
+                    //infle vista
+                    View child = getLayoutInflater().inflate(R.layout.alumnos, null);
+                    //obtener label
+                    TextView label = (TextView) child.findViewById(R.id.nombre);
+
+                    String nombre = alumno.getString("nombre");
+
+                    label.setText(nombre);
+
+                    container.addView(child);
+                }
+            }
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/appedidos/parseinnovaapp/ParseInnovaApp.java
@@ -0,0 +1,22 @@
+package com.appedidos.parseinnovaapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by jvargas on 9/29/15.
+ */
+public class ParseInnovaApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "Wjpf9K6FCcZFma68uNnVtH4rFsCkefe2iXK1oYzs", "GJ9aUc5Ndwu3w06LqkNzPHxtDO0NXJU3lwl2WsVW");
+
+    }
+}
